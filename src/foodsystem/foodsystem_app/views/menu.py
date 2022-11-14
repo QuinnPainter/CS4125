e190f5fd1dd5638ef7c72652ae6c42fcf5e71ccd
Jack Busher <61558283+jackbusher@users.noreply.github.com>
2022-11-14 12:07:21 +0000

    Update

--- src/foodsystem/foodsystem_app/views/menu.py
+++ src/foodsystem/foodsystem_app/views/menu.py
@@ -2,7 +2,38 @@ from django.shortcuts import render
 from django.views import View
 from foodsystem_app.models.product import Product
 
-class MenuView(View):
-    def get(self, request):
-        products = Product.objects.all()
-        return render(request, 'order.html', {'products':products})
+
+MENU = {}
+current_order = []
+order_price = 0
+
+        
+class MenuItem(): #component
+    #abstract function for get products
+    def getproduct():
+        return
+        
+class MenuProduct(MenuItem): #leaf
+    def __init__(self, name, price, category):
+        self.name = name
+        self.price = price
+        self.category = category
+        MENU[name] = price
+
+        #this has an implementaon of abstract func where all it does is return itself
+
+        burger = MenuProduct()
+        #burgerscategory.add(burger)
+        def getproduct(): 
+            return self    
+
+class MenuCategory(MenuItem): #composite
+        #all this does is call getproduct on all its child objects
+        #this has a list of menu items in it
+        #get product function where it just loops through the menu items
+        #then calls getproduct in menuproduct on each one
+        itemsList = MENU
+        for x in itemsList:
+            getproduct(x)
+    
+
