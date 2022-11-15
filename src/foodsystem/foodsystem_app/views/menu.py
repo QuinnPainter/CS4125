ba65c05ac4aaf9825d9b8bcc33a7bddad8c84f6e
Jack Busher <61558283+jackbusher@users.noreply.github.com>
2022-11-15 11:07:06 +0000

    Update menu.py

--- src/foodsystem/foodsystem_app/views/menu.py
+++ src/foodsystem/foodsystem_app/views/menu.py
@@ -1,38 +1,52 @@
 from django.shortcuts import render
 from django.views import View
 from foodsystem_app.models.product import Product
+from abc import abstractmethod, ABC
 
-
-MENU = {}
+MENU = []
 current_order = []
 order_price = 0
 
         
-class MenuItem(): #component
+class MenuItem(ABC): #component
     #abstract function for get products
+    @abstractmethod
     def getproduct():
         return
         
 class MenuProduct(MenuItem): #leaf
-    def __init__(self, name, price, category):
+    def __init__(self, name, price):
         self.name = name
         self.price = price
-        self.category = category
-        MENU[name] = price
 
         #this has an implementaon of abstract func where all it does is return itself
-
-        burger = MenuProduct()
         #burgerscategory.add(burger)
-        def getproduct(): 
-            return self    
+    def getproduct(self): 
+        return self    
 
 class MenuCategory(MenuItem): #composite
         #all this does is call getproduct on all its child objects
         #this has a list of menu items in it
         #get product function where it just loops through the menu items
         #then calls getproduct in menuproduct on each one
-        itemsList = MENU
-        for x in itemsList:
-            getproduct(x)
-    
+        def __init__(self):
+            self.itemsList = MENU
+
+        def getproduct(self):
+            products = []
+            for x in self.itemsList:
+                products.append(x.getproduct())           
+            return products
+
+        def addProduct(self,item):
+            self.itemsList.append(item)
+            
+
+
+burger = MenuProduct("Burger",5)
+coffee = MenuProduct("Coffee",3)
+
+food = MenuCategory()
+food.addProduct(burger)
+print(food.getproduct())
+
