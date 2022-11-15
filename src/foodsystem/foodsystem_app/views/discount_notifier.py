cf6660df21dc295efd0853c3de916903a0f795b6
Quinn Painter <quinn@geekymonkey.com>
2022-11-15 10:45:36 +0000

    Basic observer pattern for discount notifications

---
+++ src/foodsystem/foodsystem_app/views/discount_notifier.py
@@ -0,0 +1,14 @@
+class DiscountNotifier():
+    # should pass in a Discount and Customer here
+    def update():
+        pass
+
+class DiscountNotifierSMS(DiscountNotifier):
+    def update():
+        # Send an SMS message to the customer
+        pass
+
+class DiscountNotifierEmail(DiscountNotifier):
+    def update():
+        # Send an email to the customer
+        pass
